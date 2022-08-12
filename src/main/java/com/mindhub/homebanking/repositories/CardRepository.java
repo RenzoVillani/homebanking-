134c1ac43585abd7f2f7ae2fe9665bddaf02be4e
Renzo Villani <renzovillani03@gmail.com>
2022-08-12 17:50:25 -0300

    tarea_5

---
+++ src/main/java/com/mindhub/homebanking/repositories/CardRepository.java
@@ -0,0 +1,10 @@
+package com.mindhub.homebanking.repositories;
+
+import com.mindhub.homebanking.model.Card;
+import com.mindhub.homebanking.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface CardRepository extends JpaRepository<Card, Long> {
+}
